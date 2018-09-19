b19d40ee6606400f1e65eeb7d3f8329f64a16aba
Oleg Smolovyk <smolovyk.oleg@gmail.com>
2018-09-19 00:54:52 -0400

    Update: SignUpPage.js, LoginPage - forms changed to using Controlled Components concept

--- src/clientReact/components/content/LoginPage.js
+++ src/clientReact/components/content/LoginPage.js
@@ -1,27 +1,96 @@
 import React from 'react';
+import Axios from "axios";
 
 class LoginPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      userEmail: '',
+      userPassword: '',
+    };
 
-  render(){
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    // const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    //TODO: finish AJAX request
+    Axios.post('/user/login', {
+      email: this.state.userEmail,
+      password: this.state.userPassword,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
     return (
-      <div id="login" className="container">
+      <div id="LoginPage" className="container">
         <div className="row justify-content-center">
-          <div className="col-md-4">
-            <h3 className="text-center text-light">Log in</h3>
-            <form className="mt-3">
+          <div className="col-md-5">
+            <h3 className="text-center text-light mb-4">Log in</h3>
+            <form>
+              <div className="form-row">
+                <div className="form-group col-md-6">
+                  <button className="btn btn-primary btn-block btn-sm">
+                    <a className="text-light" href="https://www.facebook.com/">
+                      <i className="fa fa-facebook-square mr-2"></i>Continue with Facebook
+                    </a>
+                  </button>
+                </div>
+                <div className="form-group col-md-6">
+                  <button className="btn btn-primary btn-block btn-sm">
+                    <a className="text-light" href="https://www.google.ca/">
+                      <i className="fa fa-google-plus-square mr-2"></i>Continue with Google
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </form>
+            <p className="text-center text-light">OR</p>
+            <form className="mt-3" onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <input type="email" className="form-control" id="inputEmail" placeholder="Enter email"/>
+                <input
+                  name="userEmail"
+                  type="email"
+                  className="form-control form-control-sm"
+                  placeholder="Enter email"
+                  value={this.state.userEmail}
+                  onChange={this.handleInputChange}/>
               </div>
               <div className="form-group">
-                <input type="password" className="form-control" id="inputPassword" placeholder="Password"/>
+                <input
+                  name="userPassword"
+                  type="password"
+                  className="form-control form-control-sm"
+                  placeholder="Password"
+                  value={this.state.userPassword}
+                  onChange={this.handleInputChange}/>
               </div>
-              <button type="submit" className="btn btn-warning btn-block mt-3">Submit</button>
+              <button type="submit" className="btn btn-warning btn-sm btn-block mt-4">Submit</button>
             </form>
           </div>
         </div>
       </div>
-    );
+  );
   }
-}
+  }
 
-export default LoginPage;
+  export default LoginPage;
