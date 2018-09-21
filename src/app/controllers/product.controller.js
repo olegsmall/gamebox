a9ef056b57ae577b4a42557e16db565f715206a4
Petr0S <petr0sean@gmail.com>
2018-09-21 15:19:04 -0400

    Add product v.0.1 no return result / TO FIX GetProducts product with query filers, pagination, limits.

--- src/app/controllers/product.controller.js
+++ src/app/controllers/product.controller.js
@@ -1,26 +1,23 @@
 const ProductService = require('../services/product.services');
 
 
-exports.createProduct = async function(req, res) {
+exports.createProduct = async function(req, res, next) {
   try {
-    let newProduct = await ProductService.createProduct(req);
-    return res.status(201).json({status: 201, data: newProduct, message: 'Product Created Successfully'});
+    let newProduct = await ProductService.createProduct(req, res, next);
+    return res.status(201).json({status: 201, data: newProduct, message: 'Product created successfully'});
 
   }catch(e){
-    return res.status(400).json({status: 400, message: e.message});
+    return res.status(400).json({status: 400, error: e.message, message: 'Product was not created'});
   }
 };
 
-exports.getProduct = async function(req, res, next) {
-
-  // Check the existence of the query parameters, If the exists doesn't exists assign a default value
-  let page = req.query.page ? req.query.page : 1;
-  let limit = req.query.limit ? req.query.limit : 100;
+exports.getProducts = async function(req, res) {
 
   try {
-    let products = await ProductService.getProducts({}, page, limit);
+    // let products = await ProductService.getProducts({}, page, limit);
+    let products = await ProductService.getProducts(req);
     // Return the products list with the appropriate HTTP Status Code and Message.
-    return res.status(200).json({status: 200, data: products, message: 'Successfully Users Received'});
+    return res.status(200).json({status: 200, data: products, message: 'Products received'});
 
   } catch(e) {
     //Return an Error Response Message with Code and the Error Message.
