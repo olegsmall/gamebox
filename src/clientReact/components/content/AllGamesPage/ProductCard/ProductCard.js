fa352af23b24345ce11dde1eca7475a26d6216b8
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-09-25 01:20:05 -0400

    Update: ProductCard.js and ProductCard.scss

--- src/clientReact/components/content/AllGamesPage/ProductCard/ProductCard.js
+++ src/clientReact/components/content/AllGamesPage/ProductCard/ProductCard.js
@@ -12,26 +12,28 @@ class ProductCard extends React.Component {
     const genres = product.genres.map(genre => genre.name + ' ');
 
     return (
-      <div className="card">
-        <img className="card-img-top" src={'image/' + product.images[0]} alt={product.title}/>
-        <div className="card-body">
-          <h5 className="card-title text-center">
-            <Link to={'/product/' + product._id}>
-              {product.title}
-            </Link>
-          </h5>
-          <p className="text-center">Genre : {genres}</p>
-          <p className="text-center">Rent/Buy : {}$/{}$</p>
-          <div className="text-center">
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-          </div>
-          <div className="text-center mt-2">
-            <button type="button" className="btn btn-success">rent</button>
-            <button type="button" className="btn btn-success mr-2">buy</button>
+      <div className={"ProductCard"}>
+        <div className="card">
+          <img className="card-img-top" src={'image/' + product.images[0]} alt={product.title}/>
+          <div className="card-body">
+            <h5 className="card-title text-center">
+              <Link to={'/product/' + product._id}>
+                {product.title}
+              </Link>
+            </h5>
+            <p className="text-center">Genre : {genres}</p>
+            <p className="text-center">Rent/Buy : {}$/{}$</p>
+            <div className="text-center">
+              <i className="fa fa-star-o"></i>
+              <i className="fa fa-star-o"></i>
+              <i className="fa fa-star-o"></i>
+              <i className="fa fa-star-o"></i>
+              <i className="fa fa-star-o"></i>
+            </div>
+            <div className="text-center mt-2">
+              <button type="button" className="btn btn-success">rent</button>
+              <button type="button" className="btn btn-success mr-2">buy</button>
+            </div>
           </div>
         </div>
       </div>
