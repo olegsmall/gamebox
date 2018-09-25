3faa9eee739824906a22b005c7d767b9a4d29c33
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-09-25 01:22:02 -0400

    Update: Main.js

--- src/clientReact/components/content/MainPage.js
+++ src/clientReact/components/content/MainPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactPropTypes from 'prop-types';
 
-
 const MainPage = () => {
   return (
-    <div>
+    <div className={"MainPage"}>
       <div className="swiper-container">
         <div className="swiper-wrapper">
           <div className="swiper-slide">
@@ -20,144 +19,208 @@ const MainPage = () => {
         <div className="swiper-button-prev"></div>
         <div className="swiper-button-next"></div>
       </div>
-
-      <h2 className="mt-5 ml-5 text-light">Lorem ipsum</h2>
-      <h4 className="ml-5 text-light">Integer faucibus quam quis massa condimentum, ut vestibulum nisl gravida.
-        Suspendisse
-        potenti.</h4>
+      <h2 className="mt-5 ml-5 text-light">Recently released</h2>
+      <h4 className="ml-5 text-light">
+        Integer faucibus quam quis massa condimentum, ut vestibulum nisl gravida.
+        Suspendisse potenti.
+      </h4>
       <div className="card-deck m-5">
         <div className="card">
           <img className="card-img-top" src="image/img6.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
         <div className="card">
           <img className="card-img-top" src="image/img1.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
         <div className="card">
           <img className="card-img-top" src="image/img2.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This card has supporting text below as a natural lead-in to additional a natural
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
         <div className="card">
           <img className="card-img-top" src="image/img3.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="image/img3.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
       </div>
-
-      <h2 className="mt-5 ml-5 text-light">Lorem ipsum</h2>
-      <h4 className="ml-5 text-light">Integer faucibus quam quis massa condimentum, ut vestibulum nisl gravida.
-        Suspendisse
-        potenti.</h4>
+      <h2 className="mt-5 ml-5 text-light">Most popular</h2>
+      <h4 className="ml-5 text-light">
+        Integer faucibus quam quis massa condimentum, ut vestibulum nisl gravida.
+        Suspendisse potenti.
+      </h4>
       <div className="card-deck m-5">
         <div className="card">
           <img className="card-img-top" src="image/img1.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
         <div className="card">
           <img className="card-img-top" src="image/img4.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
         <div className="card">
           <img className="card-img-top" src="image/img5.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This card has supporting text below as a natural lead-in to additional a natural
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
         <div className="card">
           <img className="card-img-top" src="image/img6.jpg" alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title text-center">Game info</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional
-              content.</p>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <i className="fa fa-star-o"></i>
-            <button type="button" className="btn btn-success pull-right">rent</button>
-            <button type="button" className="btn btn-success pull-right mr-2">buy</button>
-          </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="image/img6.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h5 className="card-title text-center">Game's name</h5>
+              <p className="text-center">Genre : Action shooter</p>
+              <p className="text-center">Rent/Buy : 25$/300$</p>
+              <div className="text-center">
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+                <i className="fa fa-star-o"></i>
+              </div>
+              <div className="text-center mt-2">
+                <button type="button" className="btn btn-success">rent</button>
+                <button type="button" className="btn btn-success mr-2">buy</button>
+              </div>
+            </div>
         </div>
       </div>
     </div>
