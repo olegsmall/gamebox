5617725060912bb67ec90201236f3be40adc9468
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-09-25 01:20:51 -0400

    Update: AllGamesPage.js and AllGamesPage.scss

--- src/clientReact/components/content/AllGamesPage/AllGamesPage.js
+++ src/clientReact/components/content/AllGamesPage/AllGamesPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import ProductCard from './ProductCard/ProductCard';
 
+require('./AllGamesPage.scss');
+
 class AllGamesPage extends React.Component {
 
   constructor(props) {
@@ -26,14 +28,16 @@ class AllGamesPage extends React.Component {
 
   render() {
     return (
-      <div id="games" className="container mt-5 mb-5">
-        <h2 className="mt-5 ml-5 mb-5 text-light">Games</h2>
-        <div className="card-deck m-5">
+      <div className={"AllGamesPage"}>
+        <div id="games" className="container mt-5 mb-5">
+          <h2 className="mt-5 ml-5 mb-5 text-light">Games</h2>
+          <div className="card-deck m-5">
 
-          {this.state.products.map((product, index) => (
-            <ProductCard key={product._id} product={product}/>
-          ))}
+            {this.state.products.map((product, index) => (
+              <ProductCard key={product._id} product={product}/>
+            ))}
 
+          </div>
         </div>
       </div>
     );
