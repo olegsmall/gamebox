6420501049eada672b247691727d632179e6082e
Oleg Smolovyk <smolovyk.oleg@gmail.com>
2018-09-25 22:14:28 -0400

    Add: new components for user profile page

---
+++ src/clientReact/components/content/UserPage/EditProfile/EditProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+require('./EditProfile.scss');
+
+class EditProfile extends React.Component {
+
+
+  render() {
+
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default EditProfile;
