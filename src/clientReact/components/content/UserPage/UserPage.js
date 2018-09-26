c4cc501c713106d1ff69ad3636aa92c3ebea525b
YanaKravchenko <kravchenko212@gmail.com>
2018-09-26 14:19:58 -0400

    Update: UserPage.js

--- src/clientReact/components/content/UserPage/UserPage.js
+++ src/clientReact/components/content/UserPage/UserPage.js
@@ -34,6 +34,11 @@ class UserPage extends React.Component {
     this.setState({innerComponent: 'EditProfile'});
   }
 
+  goToProfile(e){
+    e.preventDefault();
+    this.setState({innerComponent: 'Profile'});
+  }
+
   render() {
 
     const firstName = (this.state.user !== null) ? this.state.user.firstName : '';
@@ -51,7 +56,7 @@ class UserPage extends React.Component {
         inner = <Orders/>;
         break;
       case 'EditProfile':
-        inner = <EditProfile/>;
+        inner = <EditProfile goToProfile={this.goToProfile.bind(this)}/>;
         break;
       case 'Articles':
         inner = <Articles/>;
@@ -66,7 +71,7 @@ class UserPage extends React.Component {
           <div className={'container'}>Hi, {firstName + ' ' + lastName}</div>
         </div>
 
-        <div className="container d-flex flex-row">
+        <div className="container d-flex flex-row mt-5">
           <div className="col-4">
             <a href="">
               <p className="text-light">
