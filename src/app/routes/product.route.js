99acebea8e2829528c083afe5b3d9a18ea4b3630
Petr0S <petr0sean@gmail.com>
2018-09-26 14:02:30 -0400

    filters product

--- src/app/routes/product.route.js
+++ src/app/routes/product.route.js
@@ -7,7 +7,6 @@ import ProductController from '../controllers/product.controller';
 
 router.get('/', ProductController.getProducts); //Get list of all products
 router.get('/:id', ProductController.getProduct); // Get One product
-// router.get('/latest', ProductController.getLatestProducts); // Get One product
 router.post('/', ProductController.createProduct); //Create product
 router.put('/:id', ProductController.updateProduct); //Update product
 router.delete('/:id', ProductController.deleteProduct); //Delete product
