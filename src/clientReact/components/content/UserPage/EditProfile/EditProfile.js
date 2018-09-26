d6b56b4a9cc57e00a43ee4521eb847f22f6c1e47
YanaKravchenko <kravchenko212@gmail.com>
2018-09-26 14:21:01 -0400

    Update: EditProfile.js, EditProfile.scss

--- src/clientReact/components/content/UserPage/EditProfile/EditProfile.js
+++ src/clientReact/components/content/UserPage/EditProfile/EditProfile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 require('./EditProfile.scss');
 
@@ -8,7 +9,71 @@ class EditProfile extends React.Component {
   render() {
 
     return (
-      <div></div>
+      <div className={'EditProfile'}>
+        <form className="mt-3" onSubmit={}>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <input type="text"
+                     className="form-control form-control-sm"
+                     id="inputName"
+                     placeholder="First Name"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <input type="text"
+                     className="form-control form-control-sm"
+                     id="inputSurname"
+                     placeholder="Last Name"/>
+            </div>
+            <div className="form-group col-md-6">
+              <input type="email"
+                     className="form-control form-control-sm"
+                     id="inputEmail"
+                     placeholder="E-mail"/>
+            </div>
+            <div className="form-group col-md-6">
+              <input type="text"
+                     className="form-control form-control-sm"
+                     id="inputPhoneNumber"
+                     placeholder="Phone number"/>
+            </div>
+          </div>
+          <div className="form-group">
+            <input type="text"
+                   className="form-control form-control-sm"
+                   id="inputAddress"
+                   placeholder="Address"/>
+          </div>
+          <button type="submit"
+                  className="btn btn-sm btn-block mb-3 buttonEditProfile">
+            Edit your info
+          </button>
+        </form>
+        <form onSubmit={}>
+          <p className="text-light">
+            Your password [ edit ]
+          </p>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <input type="password"
+                     className="form-control form-control-sm"
+                     id="inputPassword"
+                     placeholder="Your password"/>
+            </div>
+            <div className="form-group col-md-6">
+              <input type="password"
+                     className="form-control form-control-sm"
+                     id="inputConfPassword"
+                     placeholder="Your new password"/>
+            </div>
+            <button type="submit"
+                    className="btn btn-sm btn-block buttonEditProfile">
+              Edit your password
+            </button>
+            <a className="btn btn-sm btn-block buttonEditClose mb-4" href={''} onClick={this.props.goToProfile}>Close</a>
+          </div>
+        </form>
+      </div>
     );
   }
 
