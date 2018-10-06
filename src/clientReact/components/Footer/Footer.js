863a799a40af8d296a2ae85d80e61a7cd99705c0
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-06 10:48:14 -0400

    Update: MainPage.js, Footer.js

--- src/clientReact/components/Footer/Footer.js
+++ src/clientReact/components/Footer/Footer.js
@@ -9,23 +9,23 @@ const Footer = () => {
         <div className="row justify-content-center">
           <div className="col-md-6 text-center py-2">
             <div className="line text-light">
-              <div className="line-border"><a href="#" className="text-light">Home</a></div>
-              <div className="line-border"><a href="#" className="text-light">About</a></div>
-              <div className="line-border"><a href="#" className="text-light">Games</a></div>
-              <div className="line-border"><a href="#" className="text-light">Help</a></div>
-              <div><a href="#" className="text-light">Blog</a></div>
+              <div className="line-border"><a href="#" className="text-light"> Home</a></div>
+              <div className="line-border"><a href="#" className="text-light"> About</a></div>
+              <div className="line-border"><a href="#" className="text-light"> Games</a></div>
+              <div className="line-border"><a href="#" className="text-light"> Help</a></div>
+              <div><a href="#" className="text-light"> Blog</a></div>
             </div>
           </div>
         </div>
         <div className="container text-center">
           <div className="py-4">
             <div>
-              <a className="fb-ic"><i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3"> </i></a>
+              <a className="fb-ic"> <i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3"> </i></a>
               <a className="tw-ic"> <i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3"></i></a>
-              <a className="gplus-ic"><i className="fa fa-google-plus fa-lg white-text mr-md-5 mr-3"></i> </a>
-              <a className="li-ic"><i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3"></i></a>
-              <a className="ins-ic"><i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3"></i> </a>
-              <a className="pin-ic"><i className="fa fa-pinterest fa-lg white-text"></i></a>
+              <a className="gplus-ic"> <i className="fa fa-google-plus fa-lg white-text mr-md-5 mr-3"></i> </a>
+              <a className="li-ic"> <i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3"></i></a>
+              <a className="ins-ic"> <i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3"></i> </a>
+              <a className="pin-ic"> <i className="fa fa-pinterest fa-lg white-text"></i></a>
             </div>
           </div>
         </div>
