c3adecb43ee4f9bd481c0e74d0ad2ecdac5c461f
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-06 19:13:01 -0400

    Update: ArticleCard.js

--- src/clientReact/components/content/AllArticlesPage/ArticleCard/ArticleCard.js
+++ src/clientReact/components/content/AllArticlesPage/ArticleCard/ArticleCard.js
@@ -8,6 +8,7 @@ class ArticleCard extends React.Component {
     const article = this.props.article;
 
     let author = (article.author) ? article.author.firstName + ' ' + article.author.lastName : 'Anonymous';
+    let date = (article.created) ? article.created : '2018-10-07';
 
     return (
       <div className={"ArticleCard"}>
@@ -17,7 +18,7 @@ class ArticleCard extends React.Component {
               <img className="img-fluid float-md-left mr-5 imageArticle" src="image/outdoors-3106126_1280.jpg" alt="Card image"/>
               <h5 className="card-title">{article.title}</h5>
               <p className="card-text">{article.content.substring(0, 250) + '...'}</p>
-              <p className="card-text text-muted">Written by : {author}</p>
+              <p className="card-text text-muted">Written by : {author} | {date}</p>
             </div>
           </div>
         </Link>
