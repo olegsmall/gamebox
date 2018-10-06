3dbedfdbfeb7ad5103f7c476f80ecb2b9a0c88d3
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-06 02:01:31 -0400

    Update: index.ejs, AuthPanel.js, Header.js, Header.scss

--- src/clientReact/components/Header/AuthPanel.js
+++ src/clientReact/components/Header/AuthPanel.js
@@ -23,33 +23,47 @@ class AuthPanel extends React.Component {
 
     if (this.props.user) {
       buttons =
-        <div className={'d-flex flex-row flex-nowrap'}>
-          <div>
-            <Link to={'/user/'} className="user-icon">
-              <i className="fa fa-user-o fa-2x mr-3" aria-hidden="true"></i>
-            </Link>
-          </div>
-          {/*<button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-2">*/}
-          {/*<Link to={'/user/'}>Profile</Link>*/}
-          {/*</button>*/}
-          <button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-5">
-            <a onClick={this.handleLogout}>Log out</a>
-          </button>
+        <div>
+          <Link className="dropdown-item" to={'/user/'}>Your Account</Link>
+          <a className="dropdown-item" onClick={this.handleLogout}>Log out</a>
         </div>;
     } else {
       buttons =
         <div>
-          <button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-2">
-            <Link id="linkLogin" to={'/user/login'}>Log in</Link>
-          </button>
-          <button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-5">
-            <Link id="linkSignUp" to={'/user/signup'}>Sign up</Link>
-          </button>
+          <Link id="Login" className="dropdown-item" to={'/user/login'}>Log in</Link>
+          <Link id="SignUp" className="dropdown-item" to={'/user/signup'}>Sign up</Link>
         </div>;
     }
 
+//     buttons =
+//       <div className={'d-flex flex-row flex-nowrap'}>
+//         <div>
+//           <Link to={'/user/'} className="user-icon">
+//             <i className="fa fa-user-o fa-2x mr-3" aria-hidden="true"></i>
+//           </Link>
+//         </div>
+//
+//         {/*<button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-2">*/}
+//         {/*<Link to={'/user/'}>Profile</Link>*/}
+//         {/*</button>*/}
+//         <button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-5">
+//           <a onClick={this.handleLogout}>Log out</a>
+//         </button>
+//       </div>;
+//   } else {
+//   buttons =
+//     <div>
+//       <button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-2">
+//         <Link id="linkLogin" to={'/user/login'}>Log in</Link>
+//       </button>
+//       <button className="btn btn-sm btn-outline-success my-2 my-sm-0 mr-5">
+//         <Link id="linkSignUp" to={'/user/signup'}>Sign up</Link>
+//       </button>
+//     </div>;
+// }
+
     return (
-      <div>
+      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu">
         {buttons}
       </div>
     );
--- src/clientReact/components/Header/Header.js
+++ src/clientReact/components/Header/Header.js
@@ -8,63 +8,115 @@ require('./Header.scss');
 
 class Header extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      headerStickToTop: false,
+    };
+    this.headerAnimation.bind(this);
+  }
+
+  headerAnimation() {
+    const self = this;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        self.setState({headerStickToTop: true});
+      } else {
+        self.setState({headerStickToTop: false});
+      }
+    });
+  }
+
+  showSearchPanel() {
+    $(this.refs['inputSearch']).toggle(100, "linear");
+  }
+
+  componentDidMount() {
+    this.headerAnimation();
+  }
+
 
   render() {
+
+    // let headerNavClasses = "navbar navbar-expand-lg  navbar-lignt bg-light fixed-top navbar-inverse mid";
+    // headerNavClasses += this.state.headerStickToTop ? "stickytop" : "";
 
     return (
       <header className={"Header"}>
         <div className="mr-5 ml-5">
-          <nav className="navbar navbar-expand-lg  navbar fixed-top navbar-inverse mid">
+          <nav className={`navbar navbar-expand-lg  navbar-lignt fixed-top navbar-inverse mid ${this.state.headerStickToTop ? "stickytop" : ""} `}>
             <Link className="navbar-brand" to={'/'}>
-              <img src="image/logo3.PNG" width="60" height="60"/>
+              <img src="/image/logo1.png" width="40" height="40"/>
             </Link>
             <button
               className="navbar-toggler"
               type="button"
               data-toggle="collapse"
-              data-target="#navbarTogglerDemo02"
-              aria-controls="navbarTogglerDemo02"
+              data-target="#navbarToggler"
+              aria-controls="navbarToggler"
               aria-expanded="false"
               aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon-light"></span>
+              <span className="navbar-toggler-icon"><i className="fa fa-bars" aria-hidden="false"></i></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav mr-auto mt-lg-0">
+            <div className="collapse navbar-collapse" id="navbarToggler">
+              <ul className="navbar-nav mr-auto mt-sm-0">
                 <li className="nav-item">
-                  <Link className="nav-link text-light" to={'/'}>Home</Link>
+                  <Link className="nav-link" to={'/'}>Home</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link text-light" href="#">About</a>
+                  <a className="nav-link" href="#">About</a>
                 </li>
                 <li className=" nav-item">
-                  <Link className=" nav-link text-light" to={'/product'}>Games</Link>
+                  <Link className=" nav-link" to={'/product'}>Games</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link text-light" href="#">Help</a>
+                  <a className="nav-link" href="#">Help</a>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link text-light" to={'/article'}>Blog</Link>
+                  <Link className="nav-link" to={'/article'}>Blog</Link>
                 </li>
               </ul>
+              <form>
+                <input type="text" name="search" className="inputSearch" ref="inputSearch"/>
+              </form>
+              <img src="image/loupe.png" width="25" className="mr-4 btnSearch" alt="Loupe"
+                   onClick={this.showSearchPanel.bind(this)}/>
+
+              <div className="dropdown">
+                <a className="dropdown"
+                   id="dropdownMenu"
+                   data-toggle="dropdown"
+                   aria-haspopup="true"
+                   aria-expanded="false">
+                  <i className="fa fa-user-plus" aria-hidden="true"></i>
+                </a>
+                <AuthPanel
+                  history={this.props.history}
+                  logoutUser={this.props.logoutUser}
+                  loggedIn={this.props.loggedIn}
+                  user={this.props.user}
+                />
+              </div>
             </div>
-            <form className="form-inline mt-2">
-              <AuthPanel
-                history={this.props.history}
-                logoutUser={this.props.logoutUser}
-                loggedIn={this.props.loggedIn}
-                user={this.props.user}
-              />
-              <input
-                className="form-control-sm mr-sm-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"/>
-              <button
-                className="btn btn-sm btn-outline-success my-2 my-sm-0"
-                type="submit">
-                Search
-              </button>
-            </form>
+
+            {/*<form className="form-inline mt-2">*/}
+            {/*<AuthPanel*/}
+            {/*history={this.props.history}*/}
+            {/*logoutUser={this.props.logoutUser}*/}
+            {/*loggedIn={this.props.loggedIn}*/}
+            {/*user={this.props.user}*/}
+            {/*/>*/}
+            {/*<input*/}
+            {/*className="form-control-sm mr-sm-2"*/}
+            {/*type="search"*/}
+            {/*placeholder="Search"*/}
+            {/*aria-label="Search"/>*/}
+            {/*<button*/}
+            {/*className="btn btn-sm btn-outline-success my-2 my-sm-0"*/}
+            {/*type="submit">*/}
+            {/*Search*/}
+            {/*</button>*/}
+            {/*</form>*/}
           </nav>
         </div>
       </header>
