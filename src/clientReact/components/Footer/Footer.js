898808c5cae6c1bf92d52d7a9733f6d920945d9f
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-06 13:49:15 -0400

    Update: Footer.js, LoginPage.js, LoginPage.scss

--- src/clientReact/components/Footer/Footer.js
+++ src/clientReact/components/Footer/Footer.js
@@ -4,7 +4,7 @@ require('./Footer.scss');
 
 const Footer = () => {
   return (
-    <div className={'Footer'}>
+    <div className={'Footer mt-auto'}>
       <footer className="page-footer font-small teal pt-2">
         <div className="row justify-content-center">
           <div className="col-md-6 text-center py-2">
