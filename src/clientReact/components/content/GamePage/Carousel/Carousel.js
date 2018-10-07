9bb85774019a9e619806d1c19ac9bfe14709c6cd
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-07 11:57:32 -0400

    Update: index.ejs, Carousel.js, images

--- src/clientReact/components/content/GamePage/Carousel/Carousel.js
+++ src/clientReact/components/content/GamePage/Carousel/Carousel.js
@@ -1,21 +1,36 @@
 import React from 'react';
 
+require('./Carousel.scss');
+
 class Carousel extends React.Component {
   render() {
-    const product = this.props.product;
 
     return (
-      <div className="swiper-container mb-5">
-        {/*{for(let i = 0; i < imagesCarousel.length; i++){*/}
-        <div className="swiper-wrapper mt-3">
-            {/*<div className="swiper-slide">*/}
-            {/*<img className="img-fluid" src={'/image/' + product.images} alt={product.title}/>*/}
-            {/*</div>*/}
-
+      <div className={"Carousel"}>
+        <div className="swiper-container mb-3">
+          <div className="swiper-wrapper mt-3">
+            <div className="swiper-slide">
+              <img className="img-fluid imageMainGame" src="image/minecraft-1618089_1280.jpg" alt="First slide"/>
+            </div>
+            <div className="swiper-slide">
+              <img className="img-fluid imageMainGame" src="image/minecraft-529458_1280.jpg" alt="Second slide"/>
+            </div>
+            <div className="swiper-slide">
+              <img className="img-fluid imageMainGame" src="image/minecraft-1988580_1280.jpg" alt="Third slide"/>
+            </div>
+            <div className="swiper-slide">
+              <img className="img-fluid imageMainGame" src="image/minecraft-655957_1280.jpg" alt="First slide"/>
+            </div>
+            <div className="swiper-slide">
+              <img className="img-fluid imageMainGame" src="image/minecraft-1106253_1280.png" alt="Second slide"/>
+            </div>
+            <div className="swiper-slide">
+              <img className="img-fluid imageMainGame" src="image/minecraft-1746541_1280.jpg" alt="Third slide"/>
+            </div>
+          </div>
+          <div className="swiper-button-next"></div>
+          <div className="swiper-button-prev"></div>
         </div>
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-      {/*}}*/}
       </div>
     );
   }
