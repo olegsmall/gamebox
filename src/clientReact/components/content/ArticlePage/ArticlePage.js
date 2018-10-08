22c87143ef331a1687f4f62e79504b902663b85d
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-07 23:21:16 -0400

    Update: ArticleCard.js, AllArticlePage.js, ArticlePage.js

--- src/clientReact/components/content/ArticlePage/ArticlePage.js
+++ src/clientReact/components/content/ArticlePage/ArticlePage.js
@@ -24,27 +24,28 @@ class ArticlePage extends  React.Component {
   }
 
   render(){
-    const title = (this.state.article !== null) ? this.state.article.title : '';
-    const content = (this.state.article !== null) ? this.state.article.content : '';
-    const image = (this.state.article !== null) ? this.state.article.images[0] : '';
-    const author = (this.state.article !== null) ? this.state.article.author : 'Anonymous';
-    const date = (this.state.article !== null) ? this.state.article.created : '';
-    const tags = (this.state.article !== null) ? this.state.article.tags : '';
+
+    if (this.state.article === null) {
+      return null;
+    }
+
+    const {_id, title, image, content, author, created, tags} = this.state.article;
+    // const {title, content, image, author, date, tags} = this.state.article;
 
     return (
 
       <div className={"ArticlePage"}>
         <div>
-          <img className="d-block w-100 imgMain" src="image/38562f8a6333f76.jpg" alt="Article image"/>
+          <img className="d-block w-100 imgMain" src="/image/38562f8a6333f76.jpg" alt="Article image"/>
         </div>
         <div className="container articlePage">
           <div className="row">
             <div className="col">
               <h2>{title}</h2>
-              <p className="text-muted">Written by : {author} | {date}</p>
-              <img className="img-fluid mt-3" src={"/image/" + image} alt="image"/>
+              <p className="text-muted">Written by : {`${author.firstName} ${author.lastName}`} | {created.toString()}</p>
+              <img className="img-fluid mt-3" src={image} alt="image"/>
               <p className="mt-5">{content}</p>
-              <p className="my-5"><Link to={''}>{tags}</Link></p>
+              <p className="my-5"><span>{tags.join(' ')}</span></p>
             </div>
           </div>
         </div>
