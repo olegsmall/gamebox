0b81ffa8ced45e4aeffa7f2115959242463b8fd9
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-09 21:59:32 -0400

    Update: ProductCard.js, ProductCard.scss, AllGamesPage.js

--- src/clientReact/components/content/AllGamesPage/ProductCard/ProductCard.js
+++ src/clientReact/components/content/AllGamesPage/ProductCard/ProductCard.js
@@ -12,31 +12,27 @@ class ProductCard extends React.Component {
     const genres = product.genres.map(genre => genre.name + ' ');
 
     return (
-      <div className="ProductCard">
+        <div className="ProductCard col-sm-6 col-lg-4 col-xl-3 mb-4 cardDiv">
         <Link className="aBlack" to={'/product/' + product._id}>
-          <div className="card h-100 imgBorder cardAllGame">
+          <div className="card imgBody cardAllGame">
             <div className="img-fluid">
               <div className="imageStar">
                 <div className="paragraphV text-light pl-2">4,5</div>
               </div>
-              <img className="card-img-top" src={'image/' + product.images[0]} alt={product.title}/>
+              <img className="card-img-top imgBorder" src="/image/img1.jpg"/>
             </div>
-
-            <div className="card-body imgBody">
+            <div className="card-body">
               <h5 className="card-title text-center">{product.title}</h5>
               <div className="text-center">Genre : {genres}</div>
-              <p className="text-center">Rent/Buy : {}$/{}$</p>
               <div className="text-center mt-1">
-                <div>Rent<span className="btnPrice ml-2 pl-1 pr-1">  10$/day</span></div>
-                <div>Buy<span className="btnPrice ml-2 pl-1 pr-1">  55$</span></div>
-                <button className="btn-block mt-2"><a href="#" className="pr-3 pl-3">add to cart</a>
-                </button>
+                <div>Rent<span className="btnPrice ml-2 pl-1 pr-1">  {}$/day</span></div>
+                <div>Buy<span className="btnPrice ml-2 pl-1 pr-1">  {}$</span></div>
               </div>
             </div>
           </div>
         </Link>
       </div>
-    );
+     );
   }
 }
 
