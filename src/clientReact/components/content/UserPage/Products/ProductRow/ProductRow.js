0e1974f3f343a1fb31c5b071c3b8b2376202d9e3
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-11 13:20:33 -0400

    Update: comments for ProductRow.js, ProductRow.scss, Products.js, Products.scss

--- src/clientReact/components/content/UserPage/Products/ProductRow/ProductRow.js
+++ src/clientReact/components/content/UserPage/Products/ProductRow/ProductRow.js
@@ -1,3 +1,12 @@
+/**
+ * Theme: Web Project 2
+ * Description: Creating a gaming platform for exchange between players
+ * File: ProductRow.js, Product's row, the part of Products component
+ * Authors: Oleg Smolovyk, Piotr Iablocichin, Iana Kravchenko, Svitlana Melnyk
+ * Date: October 2018
+ */
+
+
 import React from 'react';
 import axios from "axios";
 import {Link} from "react-router-dom";
