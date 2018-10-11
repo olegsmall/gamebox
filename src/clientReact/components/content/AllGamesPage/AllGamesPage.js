55a2fe577ff91a7ed7b5f38a30e1097934704eb8
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-11 14:25:32 -0400

    Update: comments for AllGamesPage.js, AllGamesPage.scss, ProductCard.js, ProductCard.scss

--- src/clientReact/components/content/AllGamesPage/AllGamesPage.js
+++ src/clientReact/components/content/AllGamesPage/AllGamesPage.js
@@ -1,3 +1,11 @@
+/**
+ * Theme: Web Project 2
+ * Description: Creating a gaming platform for exchange between players
+ * File: AllGamesPage.js, all games page component
+ * Authors: Oleg Smolovyk, Piotr Iablocichin, Iana Kravchenko, Svitlana Melnyk
+ * Date: October 2018
+ */
+
 import React from 'react';
 import axios from 'axios';
 import ProductCard from './ProductCard/ProductCard';
