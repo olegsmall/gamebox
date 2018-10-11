3e482faaaeb41b1a18a174e3765572c4abbbf19f
Oleg Smolovyk <smolovyk.oleg@gmail.com>
2018-10-11 18:20:53 -0400

    Refactoring: preparing for deployment to the server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    entry: './clientReact/index.js'
+    entry: './src/clientReact/index.js'
   },
   output: {
     path: __dirname + '/public',
