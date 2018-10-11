5c127b2757c49c97669730588aecff4e0770f923
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-11 13:09:51 -0400

    Update: comments for UserPage.js, UserPage.scss

--- src/clientReact/components/content/UserPage/UserPage.js
+++ src/clientReact/components/content/UserPage/UserPage.js
@@ -1,3 +1,11 @@
+/**
+ * Theme: Web Project 2
+ * Description: Creating a gaming platform for exchange between players
+ * File: UserPage.js, User component
+ * Authors: Oleg Smolovyk, Piotr Iablocichin, Iana Kravchenko, Svitlana Melnyk
+ * Date: October 2018
+ */
+
 import React from 'react';
 import axios from 'axios';
 import Profile from './Profile/Profile';
