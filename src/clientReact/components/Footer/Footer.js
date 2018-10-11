01c5bb14a9d6806265ce809250bf1b20c62792b4
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-11 14:34:16 -0400

    Update: comments for Footer.js, Footer.scss

--- src/clientReact/components/Footer/Footer.js
+++ src/clientReact/components/Footer/Footer.js
@@ -1,3 +1,11 @@
+/**
+ * Theme: Web Project 2
+ * Description: Creating a gaming platform for exchange between players
+ * File: Footer.js, Footer component
+ * Authors: Oleg Smolovyk, Piotr Iablocichin, Iana Kravchenko, Svitlana Melnyk
+ * Date: October 2018
+ */
+
 import React from 'react';
 import {Link} from 'react-router-dom';
 
