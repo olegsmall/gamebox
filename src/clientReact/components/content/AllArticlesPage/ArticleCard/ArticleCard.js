fa60040852d068377eaefea344b7688cbf650de1
SvitlanaMelnyk <svetmelnyk@gmail.com>
2018-10-14 18:23:04 -0400

    Update: comments for ArticleCard.js, ArticleCard.scss, AllArticlesPage.js, AllArticlesPage.scss

--- src/clientReact/components/content/AllArticlesPage/ArticleCard/ArticleCard.js
+++ src/clientReact/components/content/AllArticlesPage/ArticleCard/ArticleCard.js
@@ -2,7 +2,7 @@
  * Theme: Web Project 2
  * Description: Creating a gaming platform for exchange between players
  * File: ArticleCard.js, Article's card- the part of all articles page component
- * Authors: Oleg Smolovyk, Piotr Iablocichin, Iana Kravchenko, Svitlana Melnyk
+ * Authors: Oleg Smolovyk, Iana Kravchenko, Svitlana Melnyk
  * Date: October 2018
  */
 
@@ -11,8 +11,11 @@ import {Link} from 'react-router-dom';
 
 require('./ArticleCard.scss');
 
+/**
+ * Class ArticleCard, Article's card component- the part of all articles page component.
+ */
 class ArticleCard extends React.Component {
-
+  //Class constructor using for a state props and for initializing state properties
   constructor(props){
     super(props);
     this.contentArea = React.createRef();
@@ -22,11 +25,13 @@ class ArticleCard extends React.Component {
 
   }
 
+  //Component mount method, guaranteed that component was mounted
   componentDidMount(){
     let content = this.props.article.content.replace(/<[^>]*>/g, '');
     this.contentArea.current.innerHTML = content.substring(0, 380) + '...';
   }
 
+  //Add to DOM
   render() {
     const {_id, title, image, content, author, created} = this.props.article;
     const date = new Date(created).toLocaleDateString();
